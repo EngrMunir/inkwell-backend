0855790b2df907226f9408fd1d5366ce52ddb429
munir <smsirajulmonir@gmail.com>
2025-04-15 15:32:37 +0600

    connected with database

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+// const express = require('express')
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import router from './app/routers';
+import { globalErrorHandler } from './app/middlwares/globalErrorHandler';
+import notFound from './app/middlwares/notFound';
+
+const app: Application = express();
+
+//parser
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(cookieParser());
+
+//application routes
+app.use('/api', router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Home Page');
+});
+
+app.use(globalErrorHandler);
+
+// not found route
+app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
+
+import { Server } from 'http';
+
+let server: Server;
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+
+    server = app.listen(config.port, () => {
+      console.log(`App listening on port http://localhost:${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
+
+process.on('unhandledRejection', (error) => {
+  console.log(`😡 unhandledRejection is detected, shutting down the server`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+process.on('uncaughtException', (error) => {
+  console.log(`😡 uncaughtException is detected, shutting down the server`);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
